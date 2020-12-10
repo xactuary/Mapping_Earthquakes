a5aee65add21b7d59ccec79e6c34ceb3a33251a5
xactuary <betsyw@yahoo.com>
2020-12-10 17:45:01 -0500

    added dark street view toggle

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,38 +1,73 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level. center of the Earth
+//let map = L.map('mapid').setView([30, 30], 2);
 
+// Create the map object with a center and zoom level.
+//let map = L.map("mapid", {
+//  center: [40.7, -94.5],
+ // zoom: 4
+//});
+
+//We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Add streets tile layer to ma;
+//streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map. Basemaps is the base layer object which allows
+// two different map styles to be shown in index.html file.  
+L.control.layers(baseMaps).addTo(map);
 
 
 // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/xactuary/Mapping_Earthquakes/Mapping_Multiple_Points/majorAirports.json";
 
 // Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup();
-  }
-}).addTo(map);
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
+// Creating a GeoJSON layer with the retrieved data.
+//L.geoJson(data,{
+//  onEachFeature: function(feature, layer){
+//    console.log(layer);
+
+//layer.bindPopup("<h2>"  + features.properties.faa + "</h2> <hr> <h3>Airport code " + features.properties.name + "</h3>");
+// }
+//}).addTo(map);
+
+
+
+
+
 
 //   We turn each feature into a marker on the map.
 //  pointToLayer: function(feature, latlng) {
@@ -46,12 +81,7 @@ L.geoJson(sanFranAirport, {
 
 
 
- //We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+ 
 
 //Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
